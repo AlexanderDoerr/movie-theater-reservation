6364acd8d2c5d19aa4fffb34b76d2c43299e765c
AMadmanWithABox <carter.gamble014@gmail.com>
2023-05-17 12:17:35 -0600

    Changed python mongo service to properly generate timestamp

--- mongo_service_python/main.py
+++ mongo_service_python/main.py
@@ -2,16 +2,39 @@ import grpc
 from concurrent import futures
 import order_pb2
 import order_pb2_grpc
+from google.protobuf import timestamp_pb2
+from datetime import datetime
+import time
+
+
+def get_current_timestamp():
+    now = datetime.now()
+    timestamp = timestamp_pb2.Timestamp()
+    timestamp.FromDatetime(now)
+    return timestamp
 
 
 class GreeterServicer(order_pb2_grpc.OrderServiceServicer):
     def GetOrder(self, request, context):
         uuid = "1234"
         userid = "usrid1234"
-        ticket_uuid = "tcktid1234"
-        payment_method = {"ccNum": "1234", "expDate": "1234", "cvv": "1234", "name": "1234"}
-        return order_pb2.OrderIn(uuid=uuid, userid=userid, ticket_uuid=ticket_uuid,
-                                 payment_method=payment_method, date_created=request.date_created)
+        ticket_uuids = ["tcktid1234"]
+        payment_method = order_pb2.PaymentMethod(
+            ccNum="1234",
+            expDate="1234",
+            cvv="1234",
+            name="1234"
+        )
+        now = datetime.now()
+        seconds = int(time.mktime(now.timetuple()))
+        date_created = timestamp_pb2.Timestamp(seconds=seconds)
+        return order_pb2.OrderIn(
+            uuid=uuid,
+            userid=userid,
+            ticket_uuid=ticket_uuids,
+            payment_method=payment_method,
+            date_created=date_created
+        )
 
 
 def serve():
@@ -21,7 +44,6 @@ def serve():
     server.start()
     print("Server started")
     server.wait_for_termination()
-
 
 
 if __name__ == '__main__':
