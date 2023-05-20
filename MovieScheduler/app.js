5555c19dbebabe49c8495b5d160517e6551a56f1
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-19 19:10:52 -0600

    Updated the Scheduler proto

--- MovieScheduler/app.js
+++ MovieScheduler/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-// const moviescheduler = require('./routes/movie-scheduler-route');
+const movieList = require('./routes/movie-list-route');
+
 // const sleep = require('sleep-promise');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', movieSchedulerRoute);
+app.use('/api', movieList);
+
 // app.use(moviescheduler);
 
 const port = process.env.PORT;
