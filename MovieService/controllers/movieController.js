8d9ae90c7c1c70dbd5d78317e6c3a9e2ad21a192
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-22 21:38:23 -0600

    Added a lot to the movie service

---
+++ MovieService/controllers/movieController.js
@@ -0,0 +1,58 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const PROTO_PATH = './path/to/your/protofile.proto';
+
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+const movielist_proto = grpc.loadPackageDefinition(packageDefinition).movielist;
+
+const client = new movielist_proto.MovieList('localhost:50051',
+                                            grpc.credentials.createInsecure());
+
+exports.getMovieInfo = (req, res) => {
+    client.GetMovieInfo({id: req.params.id}, function(err, response) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(response);
+        }
+    });
+};
+
+exports.getAllMovies = (req, res) => {
+    client.GetAllMovies({}, function(err, response) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(response);
+        }
+    });
+};
+
+exports.getAllShowingMovies = (req, res) => {
+    client.GetAllShowingMovies({}, function(err, response) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(response);
+        }
+    });
+};
+
+exports.updateMovie = (req, res) => {
+    client.UpdateMovie({uuid: req.params.id, is_showing: req.body.is_showing}, function(err, response) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(response);
+        }
+    });
+};
