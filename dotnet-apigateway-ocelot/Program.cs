dd10fa383b87ba2a6894017645960dcb1f9ed9d0
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-26 12:56:29 -0600

    fixed gateway

--- dotnet-apigateway-ocelot/Program.cs
+++ dotnet-apigateway-ocelot/Program.cs
@@ -1,4 +1,3 @@
-
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.LoadBalancer.LoadBalancers;
@@ -8,44 +7,21 @@ using Ocelot.Values;
 
 using Ocelot.Provider.Consul;
 using Ocelot.Provider.Polly;
-using Steeltoe.Discovery.Client;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// you can use this style...
 IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
-//IConfiguration configuration = new ConfigurationBuilder().AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true).Build();
+
 builder.Services.AddOcelot(configuration)
-.AddConsul()
-.AddPolly();
-
-
-
-/* builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
-{
-    config.SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-});
- */
-
-
-// or this style
-//builder.Configuration.AddJsonFile("ocelot.json");
-//builder.Services.AddOcelot().AddEureka().AddPolly();
-
+    .AddConsul()
+    .AddPolly();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// eureka
-builder.Services.AddDiscoveryClient(builder.Configuration);
 builder.Services.AddCors();
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -53,11 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
-
 app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
 
 // ocelot
 await app.UseOcelot();
