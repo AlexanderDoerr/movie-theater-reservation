e18872adcce0c45130f81792e476a50dfa4892b0
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-26 19:09:50 -0600

    Added Kafka

---
+++ MovieService/stream/kafka.js
@@ -0,0 +1,18 @@
+// import library
+const {Kafka} = require("kafkajs");
+const{KAFKA_USERNAME: username, KAFKA_PASSWORD: password} = process.env;
+const sasl = username && password ? {username, password, mechanism: 'plain'} : null;
+const ssl = !!sasl
+
+// create a client connection
+const kafka = new Kafka(
+    {
+        // brokers:[process.env.KAFKA_BROKER_SERVER],
+        brokers:['localhost:9092'],
+        clientId: "movie-service-producer",
+        ssl,
+        sasl
+    }
+)
+
+module.exports = kafka;
