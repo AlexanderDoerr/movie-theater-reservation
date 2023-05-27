9e5d5c04b7a0bd17ae41ac02e0c4de806a1e4a70
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-27 09:04:30 -0600

    updating gateway to eureka

--- dotnet-apigateway-ocelot/Program.cs
+++ dotnet-apigateway-ocelot/Program.cs
@@ -5,24 +5,46 @@ using Ocelot.Middleware;
 using Ocelot.Responses;
 using Ocelot.Values;
 
-// using Ocelot.Provider.Consul;
 using Ocelot.Provider.Eureka;
 using Ocelot.Provider.Polly;
+using Steeltoe.Discovery.Client;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
 
+// you can use this style...
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+//IConfiguration configuration = new ConfigurationBuilder().AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true).Build();
 builder.Services.AddOcelot(configuration)
-    .AddEureka()
-    .AddPolly();
+.AddEureka()
+.AddPolly();
+
+
+
+/* builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+});
+ */
+
+
+// or this style
+//builder.Configuration.AddJsonFile("ocelot.json");
+//builder.Services.AddOcelot().AddEureka().AddPolly();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// eureka
+builder.Services.AddDiscoveryClient(builder.Configuration);
 builder.Services.AddCors();
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -30,12 +52,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
 app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 
 // ocelot
 await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
