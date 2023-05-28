b421df1ca7dacb768731da7f3d02cda043badaf8
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-27 21:53:11 -0600

    updated Kafka file in movie service.

--- MovieService/stream/kafka.js
+++ MovieService/stream/kafka.js
@@ -1,17 +1,45 @@
+// // import library
+// const {Kafka} = require("kafkajs");
+// const{KAFKA_USERNAME: username, KAFKA_PASSWORD: password} = process.env;
+// const sasl = username && password ? {username, password, mechanism: 'plain'} : null;
+// const ssl = !!sasl
+
+// // create a client connection
+// const kafka = new Kafka(
+//     {
+//         brokers:[process.env.KAFKA_BROKER_SERVER],
+//         // brokers:['localhost:9092'],
+//         clientId: "movie-service-producer",
+//         ssl,
+//         sasl
+//     }
+// )
+
+// module.exports = kafka;
+
 // import library
 const {Kafka} = require("kafkajs");
-const{KAFKA_USERNAME: username, KAFKA_PASSWORD: password} = process.env;
+
+const {KAFKA_USERNAME: username, KAFKA_PASSWORD: password, KAFKA_BROKER_SERVER: broker} = process.env;
+
+if (!broker) {
+    throw new Error("Please define the KAFKA_BROKER_SERVER environment variable");
+}
+
 const sasl = username && password ? {username, password, mechanism: 'plain'} : null;
 const ssl = !!sasl
+
+// Logging for KafkaJS
+const logLevel = process.env.KAFKAJS_LOG_LEVEL || 'nothing';
 
 // create a client connection
 const kafka = new Kafka(
     {
-        brokers:[process.env.KAFKA_BROKER_SERVER],
-        // brokers:['localhost:9092'],
+        brokers: [broker],
         clientId: "movie-service-producer",
         ssl,
-        sasl
+        sasl,
+        logLevel
     }
 )
 
