84e9439ba12bad8accf5d8ee20e0411492c67a28
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2023-05-29 10:21:45 -0600

    fixed. route

--- MovieService/routes/schedulerRoute.js
+++ MovieService/routes/schedulerRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const movieScheduleController = require('../controllers/schedulerController');
 
-router.post('/schedule', movieScheduleController.addMovieToSchedule);
+router.post('/', movieScheduleController.addMovieToSchedule);
 router.get('/aud-schedules', movieScheduleController.getAudSchedulesByDate);
 router.put('/seat', movieScheduleController.reserveSeat);
 router.get('/showtimes', movieScheduleController.getShowtimesByDateAndMovieUuid);
